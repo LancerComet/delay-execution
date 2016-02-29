27ef07b49494c14e2f0dcdfad2e742702b14af20
Wuchenhui.[LancerComet] <wuchenhui@bilibili.com>
2016-02-29 10:01:47 +0800

    + next function can specify a function by providing function's name.

--- delay.js
+++ delay.js
@@ -4,12 +4,12 @@
 var Delay = function () {
     this._delay = {};
     this._delay.funcs = [];
-    
+
     for (var i = 0, length = arguments.length; i < length; i++) {
         if (Object.prototype.toString.call(arguments[i]) !== "[object Function]") throw new Error('Arguments must be "Function".');
         this._delay.funcs.push(arguments[i]);
     }
-    
+
 };
 
 Delay.prototype.done = function (callback) {
@@ -19,7 +19,26 @@ Delay.prototype.done = function (callback) {
     }
 };
 
-Delay.prototype.next = function () {
-    this._delay.funcs.shift();
+Delay.prototype.next = function (funcName) {
+    if (funcName && this._delay.funcs.indexOf(funcName) > -1) {
+        this._delay.funcs.splice(this._delay.funcs.indexOf(funcName), 1);
+    } else {
+        this._delay.funcs.shift();
+    }
     this._delay.funcs.length < 1 && this._delay.doneCallback();
+
 };
+
+
+/*
+    var newTask = new Delay(
+        function func1 () { setTimeout(function () { console.log("Task1: 300ms."); newTask.next(func1); }, 300)},
+        function func2 () { setTimeout(function () { console.log("Task2: 10ms."); newTask.next(func2); }, 10)},
+        function () { setTimeout(function () { console.log("Task3: 720ms."); newTask.next(); }, 720)},
+        function () { setTimeout(function () { console.log("Task4: 40ms."); newTask.next(); }, 40)}        
+    );
+
+    newTask.done(function () {
+        console.log("newTasks done!");
+    });
+ */
